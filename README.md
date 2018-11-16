a3f728810085a1ce6bfefb2b0975dfeb1af673ae
Pierre Guilmin <pierre.guilmin@mines-paristech.fr>
2018-11-16 14:32:46 -0500

    first commit\!

---
+++ README.md
@@ -0,0 +1,42 @@
+:construction: *work in progress* :construction:
+
+
+# MDS clustering
+
+**Clusterize the MDS disease by mutation and cytogenetics feature and observe clinical features distribution by cluster.**
+
+***
+
+
+## Requirements
+
+Built with R version 3.5.1
+
+- Nicola Roberts HDP R package ([GitHub link](https://github.com/nicolaroberts/hdp)) which implements the hierarchical Dirichlet process:  
+    ```R
+    devtools::install_github("nicolaroberts/hdp", build_vignettes = TRUE)
+    ```
+
+- Various R libraries:
+    - `ggplot2`
+    - `reshape2`
+    - `gridExtra`
+    - `tibble`
+    - `heatmap3`
+    - `prodlim`
+
+
+## todo list
+
+- [ ] try dataset with all genes and cytogenetics
+- [x] top genes by component
+- [ ] understand theory better
+- [ ] try other clustering method
+- [ ] play with hdp parameters
+- [ ] improve heatmap plot
+- [x] axes and title for each plot
+- [x] split genes by hotspots
+
+## temp
+
+scp -r guilminp@lski1997:/Users/guilminp/Documents/MDS .
---
+++ utils/tools.R
@@ -0,0 +1,74 @@
+#################################
+### tools.R #####################
+#################################
+#
+# This file gather some useful R tools function
+#
+
+library('ggplot2')
+
+
+get_table <- function(data, nrows, add_total_count = TRUE) {
+    # Print a custom sorted table (count and frequency) of a categorical feature
+    # → Arguments
+    #     - data           : one dimensional vector
+    #     - nrows          : if specified, return only the first nrows of the resulting table
+    #     - add_total_count: if TRUE, add a final row showing the total count
+
+    count_table <- rev(sort(table(data)))
+    prop_table  <- round(prop.table(count_table) * 100, 1)
+    
+    result <- data.frame('values' = names(count_table),
+                         'count'  = as.vector(count_table),
+                         'freq'   = paste0(as.character(prop_table), '%'))
+
+    if (! missing(nrows))
+        result = head(result, nrows)
+
+    if (add_total_count)
+        result <- rbind(result, data.frame('values' = '-- total --',
+                                            'count'  = length(data),
+                                            'freq'   = '100%'))
+
+    return (result)
+}
+
+
+set_notebook_plot_size <- function(width = 6, height = 3) {
+    # Set the plot size in jupyter notebook
+    # → Arguments
+    #     - width
+    #     - height
+
+    options(repr.plot.width = width, repr.plot.height = height)
+}
+
+
+tilt_x_label <- function(angle) {
+    # Tilt the x label by a given number of degrees
+    # → Ex : ggplot() ... + tilt_x_label(45)
+    # → Arguments
+    #     - angle: angle in degree by which to rotate x label
+
+    return (theme(axis.text.x = element_text(angle = angle, hjust = 1)))
+}
+
+
+print_and_flush <- function(string) {
+    # Print a string in stdout and flush the result (useful to print in Jupyter Notebook in for loops)
+    # → Arguments
+    #     - string: string to print
+
+    cat(string)
+    flush.console()
+}
+
+
+print_size <- function(data) {
+    # Print the size (n_row x n_column) of a given dataframe
+    # → Ex : print_size(my_data_frame) ⟹ Size of my_data_frame: 2876 x 32
+    # → Arguments
+    #     - data
+
+    cat(sprintf('Size of %s: %d x %d', deparse(substitute(data)), nrow(data), ncol(data)))
+}
